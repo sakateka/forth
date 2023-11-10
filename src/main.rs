f22dabbc5e1d0839d9b090b845394edb3d27fb4c
Sergey Kacheev <s.kacheev@gmail.com>
2023-11-10 21:48:46 +0300

    wip(forth): init evaluator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use std::collections::HashMap;
+
+use phf::phf_map;
+
+#[derive(Clone)]
+pub enum Keyword {
+    Plus,
+    Minus,
+    Mul,
+    Div,
+    Dup,
+    Over,
+    Drop,
+    Swap,
+}
+
+static KEYWORDS: phf::Map<&'static str, Keyword> = phf_map! {
+    "+" => Keyword::Plus,
+    "-" => Keyword::Minus,
+    "*" => Keyword::Mul,
+    "/" => Keyword::Div,
+    "dup" => Keyword::Dup,
+    "over" => Keyword::Over,
+    "drop" => Keyword::Drop,
+    "swap" => Keyword::Swap,
+};
+
+pub fn parse_keyword(keyword: &str) -> Option<Keyword> {
+    KEYWORDS.get(keyword).cloned()
+}
+
+pub struct Evaluator {
+    stack: Vec<i64>,
+    definitions: HashMap<String, Vec<String>>,
+}
+
+impl Evaluator {
+    pub fn new() -> Evaluator {
+        Evaluator {
+            stack: Vec::new(),
+            definitions: HashMap::new(),
+        }
+    }
+
+    pub fn process(row: impl AsRef<str>) -> Result<Vec<i64>, anyhow::Error> {
+        Ok(Vec::new())
+    }
+}
+
+impl Default for Evaluator {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 fn main() {
     println!("Here will be forth");
 }
